58d79a8422906cb9e18494df9b5c9cf88991a695
Julian Salischiker <jusalis@gmail.com>
2015-03-26 19:42:16 -0300

    Segundo commit

---
+++ src/main/java/com/uqbar-project/edu/App.java
@@ -0,0 +1,13 @@
+package com.uqbar-project.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
